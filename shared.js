a75b78e1876a80cfda9898de722a89d3a0bf0455
Maria Nichele <maria_eduardacoutinho@hotmail.com>
2021-08-02 20:45:32 -0300

    163

--- shared.js
+++ shared.js
@@ -5,6 +5,8 @@ var selectPlanButtons = document.querySelectorAll (".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector (".mobile-nav");
 
+//console.dir(backdrop.style["background-image"]);
+
 // console.dir(backdrop)
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function() {
@@ -16,22 +18,29 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 }
 
 backdrop.addEventListener("click", function(){
-    mobileNav.style.display = "none";
+    //mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal () {
     //backdrop.style.display = "none"
     //modal.style.display = "none"
-    modal.classList.remove("open");
-    modal.classList.remove("open");
+    if (modal) {
+        modal.classList.remove("open");
+    }
+    backdrop.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function(){
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block"
+    //mobileNav.style.display = "block";
+    //backdrop.style.display = "block";
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
 }); 
 
     
